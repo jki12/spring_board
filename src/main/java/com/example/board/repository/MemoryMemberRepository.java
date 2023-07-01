e100d22bc8cfe4ea4dc9301e9cf8aaacb2ee5d28
Hwangseoeun <sunny030921@gmail.com>
2023-07-01 21:28:22 +0900

    Member_repository

---
+++ src/main/java/com/example/board/repository/MemoryMemberRepository.java
@@ -0,0 +1,44 @@
+package com.example.board.repository;
+
+import com.example.board.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public Optional<Member> findByPassword(String password) {
+        return Optional.ofNullable(store.get(password));
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
